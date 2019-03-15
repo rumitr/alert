91237e245b2071e9c97bc60ad54e9228130acf87
rumit <rumit@iodparts.com>
2019-03-15 11:59:53 +0530

    initial commit

---
+++ alerts.vue
@@ -0,0 +1,18 @@
+<template>
+    <div :class="alertClasses" v-show="show">
+        <slot></slot>
+        <span class="close" @click="show = false">x</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['list'],
+    events: {
+        'add-alert':function(data) {
+            this.list.push(data);
+        }
+    }
+}
+</script>
+
---
+++ components/alert.vue
@@ -0,0 +1,30 @@
+<template>
+    <div :class="alertClasses" v-show="show">
+        <slot></slot>
+        <span class="close" @click="show = false">x</span>
+    </div>
+</template>
+<script>
+export default {
+    props: ['type'],
+    data: function() {
+        return{
+            show: true
+        }
+    },
+    computed: {
+        alertClasses: function() {
+            var type = this.type;
+            return {
+                'alert': true,
+                'alert--success': type == 'success',
+                'alert--error': type == 'error' 
+            }
+        }
+    },
+    ready: function() {
+        console.log(this);
+    }
+}
+</script>
+
